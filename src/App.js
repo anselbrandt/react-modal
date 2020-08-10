d7d2dc1dfb9dc233a3d8b69aa08ba86745c246e6
anselbrandt <ansel@anselbrandt.com>
2020-08-10 08:42:42 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState, useRef } from "react";
+import styles from "./App.module.css";
+import useOnClickOutside from "./useOnClickOutside";
+
+function App() {
+  // Create a ref that we add to the element for which we want to detect outside clicks
+  const ref = useRef();
+  // State for our modal
+  const [isModalOpen, setModalOpen] = useState(false);
+  // Call hook passing in the ref and a function to call on outside click
+  useOnClickOutside(ref, () => setModalOpen(false));
+
+  return (
+    <div className={styles.app}>
+      {isModalOpen ? (
+        <div ref={ref} className={styles.modal}>
+          Click anywhere outside to close.
+        </div>
+      ) : (
+        <button onClick={() => setModalOpen(true)}>Open Modal</button>
+      )}
+    </div>
+  );
+}
+
+export default App;
